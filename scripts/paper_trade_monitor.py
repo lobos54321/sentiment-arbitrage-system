c4bc372b2fc6ee9a4ab7e8b2dd759f0e152d10e1
artylobos <artylobos@users.noreply.github.com>
2026-04-08 10:40:59 +1000

    Widen Stage1 stop-loss to 10% to accommodate meme coin volatility

--- scripts/paper_trade_monitor.py
+++ scripts/paper_trade_monitor.py
@@ -55,7 +55,7 @@ EXECUTION_BRIDGE = PROJECT_ROOT / 'scripts' / 'execution_bridge.js'
 
 DEFAULT_STRATEGY_ID = 'notath-selective-v1'
 DEFAULT_STRATEGY_ROLE = 'selective_challenger'
-DEFAULT_STAGE1_EXIT = {'stopLossPct': 3, 'trailStartPct': 2, 'trailFactor': 0.9, 'timeoutMinutes': 120}
+DEFAULT_STAGE1_EXIT = {'stopLossPct': 10, 'trailStartPct': 2, 'trailFactor': 0.9, 'timeoutMinutes': 120}
 DEFAULT_STAGE2A = {'enabled': True, 'waitBarsAfterStop': 3, 'reboundFromRollingLowPct': 18, 'rollingLowBars': 3, 'stopLossPct': 4, 'trailStartPct': 3, 'trailFactor': 0.9, 'timeoutMinutes': 120}
 DEFAULT_STAGE3 = {
     'enabled': True,
